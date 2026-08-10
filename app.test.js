f49bb5978d91fea4e4e6b6422677f370a91348c3
Somaiah662 <arjunsomaiah66@gmail.com>
2026-08-10 11:40:40 +0530

    Initial commit

---
+++ app.test.js
@@ -0,0 +1,15 @@
+const request = require("supertest");
+const app = require("./app");
+ 
+describe("GET /hello", () => {
+ 
+    test("should return Hello World", async () => {
+ 
+        const response = await request(app).get("/hello");
+ 
+        expect(response.statusCode).toBe(200);
+ 
+        expect(response.body.message).toBe("Hello World");
+    });
+ 
+});
