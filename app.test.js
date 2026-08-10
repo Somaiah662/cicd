6f42b6ef98841ae562c89288d095653d7c865ede
Somaiah662 <arjunsomaiah66@gmail.com>
2026-08-10 14:40:35 +0530

    this is passed test

--- app.test.js
+++ app.test.js
@@ -9,7 +9,7 @@ describe("GET /hello", () => {
  
         expect(response.statusCode).toBe(200);
  
-        expect(response.body.message).toBe("Hello world");
+        expect(response.body.message).toBe("Hello World");
     });
  
 });
